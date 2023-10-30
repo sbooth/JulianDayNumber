6aadb9a20d53ff6ee88b0671fd01dca6679b8eaa
Stephen F. Booth <me@sbooth.org>
2023-10-29 22:18:50 -0500

    Additional JD tests

--- Tests/JulianDayNumberTests/JDTests.swift
+++ Tests/JulianDayNumberTests/JDTests.swift
@@ -26,6 +26,48 @@ final class JDTests: XCTestCase {
 		XCTAssertEqual(calendarDateToJulianDate(year: -1000, month: 2, day: 29), 1355866.5)
 		XCTAssertEqual(calendarDateToJulianDate(year: -1001, month: 8, day: 17.9), 1355671.4)
 		XCTAssertEqual(calendarDateToJulianDate(year: -4712, month: 1, day: 1.5), 0)
+
+		XCTAssertTrue(julianDateToCalendarDate(2451545) == (2000, 1, 1, 12, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(2451179.5) == (1999, 1, 1, 0, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(2446822.5) == (1987, 1, 27, 0, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(2446966) == (1987, 6, 19, 12, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(2447187.5) == (1988, 1, 27, 0, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(2447332.0) == (1988, 6, 19, 12, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(2415020.5) == (1900, 1, 1, 0, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(2305447.5) == (1600, 1, 1, 0, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(2305812.5) == (1600, 12, 31, 0, 0, 0))
+//		XCTAssertTrue(julianDateToCalendarDate(2026871.8) == (837, 4, 10, 7, 12, 0))
+		// Account for floating-point error since 2026871.8 is not perfectly representable by a double (real value is 2026871.80000000005)
+		var calendarDate = julianDateToCalendarDate(2026871.8)
+		XCTAssertTrue((calendarDate.year, calendarDate.month, calendarDate.day) == (837, 4, 10))
+		var fractionalDay = timeToFractionalDay(hour: calendarDate.hour, minute: calendarDate.minute, second: calendarDate.second)
+		XCTAssertEqual(fractionalDay, 0.3, accuracy: 0.000005) // ≈1/2 second accuracy check
+		XCTAssertTrue(julianDateToCalendarDate(1676496.5) == (-123, 12, 31, 0, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(1676497.5) == (-122, 1, 1, 0, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(1356001) == (-1000, 7, 12, 12, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(1355866.5) == (-1000, 2, 29, 0, 0, 0))
+//		XCTAssertTrue(julianDateToCalendarDate(1355671.4) == (-1001, 8, 17, 21, 36, 0))
+		// Account for floating-point error since 1355671.4 is not perfectly representable by a double (real value is 1355671.39999999991)
+		calendarDate = julianDateToCalendarDate(1355671.4)
+		XCTAssertTrue((calendarDate.year, calendarDate.month, calendarDate.day) == (-1001, 8, 17))
+		fractionalDay = timeToFractionalDay(hour: calendarDate.hour, minute: calendarDate.minute, second: calendarDate.second)
+		XCTAssertEqual(fractionalDay, 0.9, accuracy: 0.000005)  // ≈1/2 second accuracy check
+		XCTAssertTrue(julianDateToCalendarDate(0) == (-4712, 1, 1, 12, 0, 0))
+
+		XCTAssertEqual(calendarDateToJulianDate(year: -4713, month: 12, day: 31, hour: 12), -1)
+		XCTAssertEqual(calendarDateToJulianDate(year: -4712, month: 1, day: 1), -0.5)
+		XCTAssertEqual(calendarDateToJulianDate(year: -4712, month: 1, day: 1, hour: 12), 0)
+		XCTAssertEqual(calendarDateToJulianDate(year: -4712, month: 1, day: 2), 0.5)
+		XCTAssertEqual(calendarDateToJulianDate(year: -4712, month: 1, day: 2, hour: 12), 1)
+
+		XCTAssertTrue(julianDateToCalendarDate(-1) == (-4713, 12, 31, 12, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(-0.5) == (-4712, 1, 1, 0, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(0) == (-4712, 1, 1, 12, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(0.5) == (-4712, 1, 2, 0, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(1) == (-4712, 1, 2, 12, 0, 0))
+
+		XCTAssertTrue(julianDateToCalendarDate(-1.75) == (-4713, 12, 30, 18, 0, 0))
+		XCTAssertTrue(julianDateToCalendarDate(1.75) == (-4712, 1, 3, 6, 0, 0))
 	}
 
 	func testJDLimits() {
