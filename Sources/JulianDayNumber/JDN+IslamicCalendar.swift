04e90a444997c033db2dde9823e3d5c039dfc123
Stephen F. Booth <me@sbooth.org>
2023-11-02 15:34:48 -0500

    Add JulianDayNumber and JulianDayNumberConverting

--- Sources/JulianDayNumber/JDN+IslamicCalendar.swift
+++ Sources/JulianDayNumber/JDN+IslamicCalendar.swift
@@ -18,87 +18,75 @@ let islamicCalendarCycleYears = 30
 /// A cycle in the Islamic calendar consists of 19 years of 354 days and 11 leap years of 355 days.
 let islamicCalendarCycleDays = 10631
 
-/// The earliest supported Julian day number using the Islamic calendar.
-///
-/// This JDN corresponds to -9999-01-01 12:00:00 in the Islamic calendar
-let earliestSupportedIslamicCalendarJDN = -1595227
+extension IslamicCalendar: JulianDayNumberConverting {
+	/// Converts a year, month, and day in the Islamic calendar to a Julian day number.
+	///
+	/// - note: No validation checks are performed on the date values.
+	///
+	/// - parameter Y: A year number between `-9999` and `99999`.
+	/// - parameter M: A month number between `1` (Muharram) and `12` (Dhú’l-Hijjab).
+	/// - parameter D: A day number between `1` and the maximum number of days in month `M` for year `Y`.
+	///
+	/// - returns: The Julian day number corresponding to the specified year, month, and day.
+	public static func dateToJulianDayNumber(year Y: Int, month M: Int, day D: Int) -> JulianDayNumber {
+		var Y = Y
+		var ΔcalendarCycles = 0
 
-/// The latest supported Julian day number using the Islamic calendar.
-///
-/// This JDN corresponds to 99999-12-29 12:00:00 in the Islamic calendar
-let latestSupportedIslamicCalendarJDN = 37384751
+		// Richards' algorithm is only valid for positive JDNs.
+		// JDN 0 is -5498-08-16 in the proleptic Islamic calendar.
+		// Adjust the year of earlier dates forward in time by a multiple of
+		// the calendar's cycle before calculating the JDN, and then translate
+		// the result backward in time by the period of adjustment.
+		if Y < -5498 || (Y == -5498 && (M < 8 || (M == 8 && D < 16))) {
+			ΔcalendarCycles = (-5498 - Y) / islamicCalendarCycleYears + 1
+			Y += ΔcalendarCycles * islamicCalendarCycleYears
+		}
 
-/// Converts a year, month, and day in the Islamic calendar to a Julian day number.
-///
-/// The Julian day number (JDN) is the integer assigned to a whole solar day in the Julian day count starting from noon Universal Time,
-/// with JDN 0 assigned to the day starting at noon on Monday, January 1, 4713 BC (-4712-01-01 12:00:00) in the proleptic Julian calendar.
-/// This date is Shaabán 17, 5499 B.H. (-5498-08-16 12:00:00) in the proleptic Islamic calendar.
-///
-/// - note: No validation checks are performed on the date values.
-///
-/// - parameter Y: A year number between `-9999` and `99999`.
-/// - parameter M: A month number between `1` (Muharram) and `12` (Dhú’l-Hijjab).
-/// - parameter D: A day number between `1` and the maximum number of days in month `M` for year `Y`.
-///
-/// - returns: The Julian day number corresponding to the specified year, month, and day.
-public func islamicCalendarDateToJulianDayNumber(year Y: Int, month M: Int, day D: Int) -> Int {
-	var Y = Y
-	var ΔcalendarCycles = 0
+		let h = M - m
+		let g = Y + y - (n - h) / n
+		let f = (h - 1 + n) % n
+		let e = (p * g + q) / r + D - 1 - j
+		var J = e + (s * f + t) / u
 
-	// Richards' algorithm is only valid for positive JDNs.
-	// JDN 0 is -5498-08-16 in the proleptic Islamic calendar.
-	// Adjust the year of earlier dates forward in time by a multiple of
-	// the calendar's cycle before calculating the JDN, and then translate
-	// the result backward in time by the period of adjustment.
-	if Y < -5498 || (Y == -5498 && (M < 8 || (M == 8 && D < 16))) {
-		ΔcalendarCycles = (-5498 - Y) / islamicCalendarCycleYears + 1
-		Y += ΔcalendarCycles * islamicCalendarCycleYears
+		if ΔcalendarCycles > 0 {
+			J -= ΔcalendarCycles * islamicCalendarCycleDays
+		}
+
+		return J
 	}
 
-	let h = M - m
-	let g = Y + y - (n - h) / n
-	let f = (h - 1 + n) % n
-	let e = (p * g + q) / r + D - 1 - j
-	var J = e + (s * f + t) / u
+	/// Converts a Julian day number to a year, month, and day in the Islamic calendar.
+	///
+	/// - parameter J: A Julian day number between `-1595227` and `37384751`.
+	///
+	/// - returns: The year, month, and day corresponding to the specified Julian day number.
+	public static func julianDayNumberToDate(_ J: JulianDayNumber) -> (year: Int, month: Int, day: Int) {
+		var J = J
+		var ΔcalendarCycles = 0
 
-	if ΔcalendarCycles > 0 {
-		J -= ΔcalendarCycles * islamicCalendarCycleDays
+		// Richards' algorithm is only valid for positive JDNs.
+		// Adjust negative JDNs forward in time by a multiple of
+		// the calendar's cycle before calculating the JDN, and then translate
+		// the result backward in time by the period of adjustment.
+		if J < 0 {
+			ΔcalendarCycles = -J / islamicCalendarCycleDays + 1
+			J += ΔcalendarCycles * islamicCalendarCycleDays
+		}
+
+		let f = J + j
+		let e = r * f + v
+		let g = (e % p) / r
+		let h = u * g + w
+		let D = (h % s) / u + 1
+		let M = ((h / s + m) % n) + 1
+		var Y = e / p - y + (n + m - M) / n
+
+		if ΔcalendarCycles > 0 {
+			Y -= ΔcalendarCycles * islamicCalendarCycleYears
+		}
+
+		return (Y, M, D)
 	}
-
-	return J
-}
-
-/// Converts a Julian day number to a year, month, and day in the Islamic calendar.
-///
-/// - parameter J: A Julian day number between `-1595227` and `37384751`.
-///
-/// - returns: The year, month, and day corresponding to the specified Julian day number.
-public func julianDayNumberToIslamicCalendarDate(_ J: Int) -> (year: Int, month: Int, day: Int) {
-	var J = J
-	var ΔcalendarCycles = 0
-
-	// Richards' algorithm is only valid for positive JDNs.
-	// Adjust negative JDNs forward in time by a multiple of
-	// the calendar's cycle before calculating the JDN, and then translate
-	// the result backward in time by the period of adjustment.
-	if J < 0 {
-		ΔcalendarCycles = -J / islamicCalendarCycleDays + 1
-		J += ΔcalendarCycles * islamicCalendarCycleDays
-	}
-
-	let f = J + j
-	let e = r * f + v
-	let g = (e % p) / r
-	let h = u * g + w
-	let D = (h % s) / u + 1
-	let M = ((h / s + m) % n) + 1
-	var Y = e / p - y + (n + m - M) / n
-
-	if ΔcalendarCycles > 0 {
-		Y -= ΔcalendarCycles * islamicCalendarCycleYears
-	}
-
-	return (Y, M, D)
 }
 
 // Constants for Islamic calendar conversions
--- Sources/JulianDayNumber/JDN.swift
+++ Sources/JulianDayNumber/JDN.swift
@@ -9,188 +9,29 @@ import Foundation
 /// A Julian day number.
 ///
 /// The Julian day number (JDN) is the integer assigned to a whole solar day in the Julian day count starting from noon Universal Time,
-/// with JDN 0 assigned to the day starting at noon on Monday, January 1, 4713 BC (-4712-01-01 12:00:00) in the proleptic Julian calendar.
-public struct JDN: Hashable {
-	/// The Julian day number.
-	public let julianDayNumber: Int
+/// with JDN 0 assigned to the day starting at noon on Monday, January 1, 4713 BC in the proleptic Julian calendar.
+///
+/// JDN 0 is November 24, 4714 B.C. in the proleptic Gregorian calendar.
+/// JDN 0 is Shaabán 17, 5499 B.H. in the proleptic Islamic calendar.
+public typealias JulianDayNumber = Int
 
-	/// Creates a Julian day number value initialized to the specified Julian day number.
-	///
-	/// - parameter julianDayNumber: A Julian day number.
-	public init(_ julianDayNumber: Int) {
-		self.julianDayNumber = julianDayNumber
-	}
-}
-
-extension JDN {
-	/// A system defining the beginning, length, and divisions of a year.
-	public enum Calendar {
-		/// The Julian calendar.
-		///
-		/// The earliest supported date for the Julian calendar is `-9999-01-01` and the latest supported date is `99999-12-31`.
-		/// The earliest supported Julian day number for the Julian calendar is `-1931076` and the latest supported Julian day number is `38246057`.
-		case julian
-		/// The Gregorian calendar.
-		///
-		/// The earliest supported date for the Gregorian calendar is `-9999-01-01` and the latest supported date is `99999-12-31`.
-		/// The earliest supported Julian day number for the Gregorian calendar is `-1930999` and the latest supported Julian day number is `38245309`.
-		case gregorian
-		/// The Julian calendar for dates before 1582-10-15 and the Gregorian calendar for later dates.
-		///
-		/// The earliest supported date for the Julian calendar is `-9999-01-01` and the latest supported date for the Gregorian calendar is `99999-12-31`.
-		/// The earliest supported Julian day number for the Julian calendar is `-1931076` and the latest supported Julian day number for the Gregorian calendar is `38245309`.
-		case julianGregorian
-		/// The civil Islamic calendar.
-		///
-		/// The earliest supported date for the Islamic calendar is `-9999-01-01` and the latest supported date is `99999-12-29`.
-		/// The earliest supported Julian day number for the Islamic calendar is `-1595227` and the latest supported Julian day number is `37384751`.
-		case islamic
-	}
-
-	/// Creates a Julian day number value initialized to the specified year, month, and day in `calendar`.
+/// Julian day number to date conversion.
+public protocol JulianDayNumberConverting {
+	/// Converts a year, month, and day to a Julian day number and returns the result.
 	///
 	/// - note: No validation checks are performed on the date values.
 	///
-	/// - parameter Y: A year number between `-9999` and `99999`.
-	/// - parameter M: A month number between `1` and the maximum number of months in year `Y`.
-	/// - parameter D: A day number between `1` and the maximum number of days in month `M` for year `Y`.
-	/// - parameter calendar: The calendar used to interpret the year, month, and date.
-	public init(year Y: Int, month M: Int, day D: Int, calendar: Calendar = .julianGregorian) {
-		switch calendar {
-		case .julian:
-			self.init(julianCalendarDateToJulianDayNumber(year: Y, month: M, day: D))
-		case .gregorian:
-			self.init(gregorianCalendarDateToJulianDayNumber(year: Y, month: M, day: D))
-		case .julianGregorian:
-			if (Y, M, D) < gregorianCalendarChangeoverDate {
-				self.init(julianCalendarDateToJulianDayNumber(year: Y, month: M, day: D))
-			} else {
-				self.init(gregorianCalendarDateToJulianDayNumber(year: Y, month: M, day: D))
-			}
-		case .islamic:
-			self.init(islamicCalendarDateToJulianDayNumber(year: Y, month: M, day: D))
-		}
-	}
-
-	/// Converts the Julian day number to a year, month, and day in `calendar`.
+	/// - parameter Y: A year number.
+	/// - parameter M: A month number.
+	/// - parameter D: A day number.
 	///
-	/// - parameter calendar: The calendar used to interpret the date.
+	/// - returns: The Julian day number corresponding to the specified year, month, and day.
+	static func dateToJulianDayNumber(year Y: Int, month M: Int, day D: Int) -> JulianDayNumber
+
+	/// Converts a Julian day number to a year, month, and day and returns the result.
+	///
+	/// - parameter J: A Julian day number.
 	///
 	/// - returns: The year, month, and day corresponding to the specified Julian day number.
-	public func toDate(_ calendar: Calendar = .julianGregorian) -> (year: Int, month: Int, day: Int) {
-		switch calendar {
-		case .julian:
-			return julianDayNumberToJulianCalendarDate(julianDayNumber)
-		case .gregorian:
-			return julianDayNumberToGregorianCalendarDate(julianDayNumber)
-		case .julianGregorian:
-			if julianDayNumber < gregorianCalendarChangeoverJDN {
-				return julianDayNumberToJulianCalendarDate(julianDayNumber)
-			} else {
-				return julianDayNumberToGregorianCalendarDate(julianDayNumber)
-			}
-		case .islamic:
-			return julianDayNumberToIslamicCalendarDate(julianDayNumber)
-		}
-	}
-}
-
-extension JDN: ExpressibleByIntegerLiteral {
-	/// Creates a Julian day number value initialized to the specified Julian day number.
-	///
-	/// - parameter value: A Julian day number.
-	public init(integerLiteral value: IntegerLiteralType) {
-		self = .init(value)
-	}
-}
-
-extension JDN: CustomStringConvertible {
-	/// Returns a string representation of `self`.
-	public var description: String {
-		"JDN \(julianDayNumber)"
-	}
-}
-
-extension JDN: CustomDebugStringConvertible {
-	/// Returns a string representation of `self` suitable for debugging.
-	public var debugDescription: String {
-		let d = toDate()
-		return String(format: "JDN %d (%02d-%02d-%02d)", julianDayNumber, d.year, d.month, d.day)
-	}
-}
-
-// MARK: Operator Overloads
-
-extension JDN {
-	/// Returns `true` if the Julian day number `J` is equal to `julianDayNumber`.
-	///
-	/// - parameter J: A `JDN` instance.
-	/// - parameter julianDayNumber: A Julian day number.
-	///
-	/// - returns: `true` if `J.julianDayNumber` is equal to `julianDayNumber`.
-	static public func ==(J: JDN, julianDayNumber: Int) -> Bool {
-		J.julianDayNumber == julianDayNumber
-	}
-
-	/// Returns `true` if the Julian day number `J` is not equal to `julianDayNumber`.
-	///
-	/// - parameter J: A `JDN` instance.
-	/// - parameter julianDayNumber: A Julian day number.
-	///
-	/// - returns: `true` if  `J.julianDayNumber` is not equal to `julianDayNumber`.
-	static public func !=(J: JDN, julianDayNumber: Int) -> Bool {
-		J.julianDayNumber != julianDayNumber
-	}
-
-	/// Returns `true` if the Julian day number `J` is less than `julianDayNumber`.
-	///
-	/// - parameter J: A `JDN` instance.
-	/// - parameter julianDayNumber: A Julian day number.
-	///
-	/// - returns: `true` if  `J.julianDayNumber` is less than `julianDayNumber`.
-	static public func <(J: JDN, julianDayNumber: Int) -> Bool {
-		J.julianDayNumber < julianDayNumber
-	}
-
-	/// Returns `true` if the Julian day number `J` is less than or equal to `julianDayNumber`.
-	///
-	/// - parameter J: A `JDN` instance.
-	/// - parameter julianDayNumber: A Julian day number.
-	///
-	/// - returns: `true` if `J.julianDayNumber` is less than or equal to `julianDayNumber`.
-	static public func <=(J: JDN, julianDayNumber: Int) -> Bool {
-		J.julianDayNumber <= julianDayNumber
-	}
-
-	/// Returns `true` if the Julian day number `J` is greater than `julianDayNumber`.
-	///
-	/// - parameter J: A `JDN` instance.
-	/// - parameter julianDayNumber: A Julian day number.
-	///
-	/// - returns: `true` if  `J.julianDayNumber` is greater than `julianDayNumber`.
-	static public func >(J: JDN, julianDayNumber: Int) -> Bool {
-		J.julianDayNumber > julianDayNumber
-	}
-
-	/// Returns `true` if the Julian day number `J` is greater than or equal to `julianDayNumber`.
-	///
-	/// - parameter J: A `JDN` instance.
-	/// - parameter julianDayNumber: A Julian day number.
-	///
-	/// - returns: `true` if  `J.julianDayNumber` is greater than or equal to `julianDayNumber`.
-	static public func >=(J: JDN, julianDayNumber: Int) -> Bool {
-		J.julianDayNumber >= julianDayNumber
-	}
-}
-
-extension JDN {
-	/// Returns `true` if the Julian day number `J` is equal to `date`.
-	///
-	/// - parameter J: A `JDN` instance.
-	/// - parameter date: A year, month, and day.
-	///
-	/// - returns: `true` if `J.toDate()` is equal to `date`.
-	static public func ==(J: JDN, date: (year: Int, month: Int, day: Int)) -> Bool {
-		J.toDate() == date
-	}
+	static func julianDayNumberToDate(_ J: JulianDayNumber) -> (year: Int, month: Int, day: Int)
 }
