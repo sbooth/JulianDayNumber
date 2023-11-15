2eb6fce96bb03a4bac46ff3d9b45f7af64cab94c
Stephen F. Booth <me@sbooth.org>
2023-11-14 22:46:09 -0600

    Consolidate source files

--- Sources/JulianDayNumber/GregorianCalendar.swift
+++ Sources/JulianDayNumber/GregorianCalendar.swift
@@ -173,3 +173,28 @@ public struct GregorianCalendar {
 		return (M, D)
 	}
 }
+
+extension GregorianCalendar: JulianDayNumberConverting {
+	/// A date in the Gregorian calendar consists of a year, month, and day.
+	public typealias DateType = JulianCalendar.DateType
+
+	/// The intercalating cycle of the Gregorian calendar is 303 common years of 365 days and 97 leap years of 366 days.
+	static let intercalatingCycle = (years: 400, days: 146097)
+
+	/// The date for Julian day number zero in the proleptic Gregorian calendar.
+	static let julianDayNumberZero = (year: -4713, month: 11, day: 24)
+
+	/// Algorithm parameters for Gregorian calendar conversions.
+	static let conversionParameters = JDNConversionParameters(y: 4716, j: 1401, m: 2, n: 12, r: 4, p: 1461, q: 0, v: 3, u: 5, s: 153, t: 2, w: 2)
+
+	/// Gregorian intercalating parameters for Gregorian calendar conversions.
+	static let gregorianIntercalatingParameters = JDNGregorianIntercalatingParameters(A: 184, B: 274277, C: -38)
+
+	public static func julianDayNumberFromDate(_ date: DateType) -> JulianDayNumber {
+		jdnFromDate(date, conversionParameters: conversionParameters, gregorianIntercalatingParameters: gregorianIntercalatingParameters, jdnZero: julianDayNumberZero, intercalatingCycle: intercalatingCycle)
+	}
+
+	public static func dateFromJulianDayNumber(_ J: JulianDayNumber) -> DateType {
+		dateFromJDN(J, conversionParameters: conversionParameters, gregorianIntercalatingParameters: gregorianIntercalatingParameters, intercalatingCycle: intercalatingCycle)
+	}
+}
--- Sources/JulianDayNumber/KhwarizmianCalendar.swift
+++ Sources/JulianDayNumber/KhwarizmianCalendar.swift
@@ -78,3 +78,25 @@ public struct KhwarizmianCalendar {
 		return monthLengths[M - 1]
 	}
 }
+
+extension KhwarizmianCalendar: JulianDayNumberConverting {
+	/// A date in the Khwarizmian calendar consists of a year, month, and day.
+	public typealias DateType = (year: Year, month: Month, day: Day)
+
+	/// The intercalating cycle of the Khwarizmian calendar is 1 year of 365 days.
+	static let intercalatingCycle = (years: 1, days: 365)
+
+	/// The date for Julian day number zero in the proleptic Khwarizmian calendar.
+	static let julianDayNumberZero = (year: -5348, month: 11, day: 18)
+
+	/// Algorithm parameters for Khwarizmian calendar conversions.
+	static let conversionParameters = JDNConversionParameters(y: 5348, j: 317, m: 0, n: 13, r: 1, p: 365, q: 0, v: 0, u: 1, s: 30, t: 0, w: 0)
+
+	public static func julianDayNumberFromDate(_ date: DateType) -> JulianDayNumber {
+		jdnFromDate(date, conversionParameters: conversionParameters, jdnZero: julianDayNumberZero, intercalatingCycle: intercalatingCycle)
+	}
+
+	public static func dateFromJulianDayNumber(_ J: JulianDayNumber) -> DateType {
+		dateFromJDN(J, conversionParameters: conversionParameters, intercalatingCycle: intercalatingCycle)
+	}
+}
