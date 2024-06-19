30414f477183452fcd3e323145d0280b7b142b0f
Stephen Booth <me@sbooth.org>
2024-06-18 23:11:29 -0500

    Make j2000 immutable (#38)

--- Sources/JulianDayNumber/Date+JD.swift
+++ Sources/JulianDayNumber/Date+JD.swift
@@ -1,5 +1,5 @@
 //
-// Copyright © 2021-2023 Stephen F. Booth <me@sbooth.org>
+// Copyright © 2021-2024 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/JulianDayNumber
 // MIT license
 //
@@ -48,5 +48,5 @@ extension Date {
 	/// | -- | --- | --- |
 	/// | TT | 2000-01-01 12:00:00 | 2451545.0 |
 	/// | UTC | 2000-01-01 11:58:55.816 | 2451544.9992571296 |
-	public static var j2000 = Date(julianDate: J2000JD_UTC)
+	public static let j2000 = Date(julianDate: J2000JD_UTC)
 }
