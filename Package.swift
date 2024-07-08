c04d69805fb699dc51a4dcf0e5acbb27931e5a52
Stephen F. Booth <me@sbooth.org>
2024-07-07 21:37:12 -0500

    Update formatting

--- Package.swift
+++ Package.swift
@@ -6,18 +6,19 @@ import PackageDescription
 let package = Package(
 	name: "JulianDayNumber",
 	products: [
-		// Products define the executables and libraries a package produces, making them visible to other packages.
 		.library(
 			name: "JulianDayNumber",
-			targets: ["JulianDayNumber"]),
+			targets: [
+				"JulianDayNumber",
+			]),
 	],
 	targets: [
-		// Targets are the basic building blocks of a package, defining a module or a test suite.
-		// Targets can depend on other targets in this package and products from dependencies.
 		.target(
 			name: "JulianDayNumber"),
 		.testTarget(
 			name: "JulianDayNumberTests",
-			dependencies: ["JulianDayNumber"]),
+			dependencies: [
+				"JulianDayNumber",
+			]),
 	]
 )
