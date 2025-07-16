4bef7e2bb01e0148f091000ee8afc4f25cb8cceb
Stephen F. Booth <me@sbooth.org>
2025-07-16 08:39:27 -0500

    Add congruence tests for Hebrew and Persian calendars

--- Tests/JulianDayNumberTests/CongruenceTests.swift
+++ Tests/JulianDayNumberTests/CongruenceTests.swift
@@ -1,5 +1,5 @@
 //
-// Copyright © 2021-2023 Stephen F. Booth <me@sbooth.org>
+// Copyright © 2021-2025 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/JulianDayNumber
 // MIT license
 //
@@ -17,10 +17,12 @@ final class CongruenceTests: XCTestCase {
 		XCTAssertEqual(FrenchRepublicanCalendar.julianDayNumberFrom(year: 1, month: 1, day: 1), GregorianCalendar.julianDayNumberFrom(year: 1792, month: 9, day: 22))
 		// Strictly speaking the Gregorian calendar epoch is January 1, 1 CE in the Julian calendar
 		XCTAssertEqual(GregorianCalendar.julianDayNumberFrom(year: 1, month: 1, day: 1), JulianCalendar.julianDayNumberFrom(year: 1, month: 1, day: 3))
+		XCTAssertEqual(HebrewCalendar.julianDayNumberFrom(year: 1, month: 1, day: 1), JulianCalendar.julianDayNumberFrom(year: -3760, month: 10, day: 7))
 		XCTAssertEqual(IslamicCalendar.julianDayNumberFrom(year: 1, month: 1, day: 1), JulianCalendar.julianDayNumberFrom(year: 622, month: 7, day: 16))
 		XCTAssertEqual(KhwarizmianCalendar.julianDayNumberFrom(year: 1, month: 1, day: 1), JulianCalendar.julianDayNumberFrom(year: 632, month: 6, day: 21))
 		XCTAssertEqual(MacedonianCalendar.julianDayNumberFrom(year: 1, month: 1, day: 1), JulianCalendar.julianDayNumberFrom(year: -311, month: 9, day: 1))
 		XCTAssertEqual(MayaCalendar.julianDayNumberFromLongCount(baktun: 0, katun: 0, tun: 0, uinal: 0, kin: 0), JulianCalendar.julianDayNumberFrom(year: -3113, month: 9, day: 6))
+		XCTAssertEqual(PersianCalendar.julianDayNumberFrom(year: 1, month: 1, day: 1), JulianCalendar.julianDayNumberFrom(year: 632, month: 6, day: 16))
 		XCTAssertEqual(SakaCalendar.julianDayNumberFrom(year: 1, month: 1, day: 1), JulianCalendar.julianDayNumberFrom(year: 79, month: 3, day: 24))
 		XCTAssertEqual(SyrianCalendar.julianDayNumberFrom(year: 1, month: 1, day: 1), JulianCalendar.julianDayNumberFrom(year: -311, month: 10, day: 1))
 	}
