9630c43e3684be28b757aade2feb92755db5808e
Stephen F. Booth <me@sbooth.org>
2025-07-18 10:06:06 -0500

    Update year

--- Tests/JulianDayNumberTests/PersianCalendarTests.swift
+++ Tests/JulianDayNumberTests/PersianCalendarTests.swift
@@ -1,5 +1,5 @@
 //
-// Copyright © 2021-2024 Stephen F. Booth <me@sbooth.org>
+// Copyright © 2021-2025 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/JulianDayNumber
 // MIT license
 //
